699a160c805a8d5a5c90ab82a9f10bf82a2d1570
Hamin <hamin2633@naver.com>
2023-02-13 22:49:25 +0900

    flask로 변경

---
+++ final_proj/ai.py
@@ -0,0 +1,78 @@
+import os
+import cv2
+import torch
+from image_process import load_img, save_img
+import torch.nn.functional as F
+from skimage import img_as_ubyte
+
+from model import Restormer
+
+parameters = {'inp_channels': 3, 'out_channels': 3, 'dim': 48, 'num_blocks': [4, 6, 6, 8], 'num_refinement_blocks': 4, 'heads': [
+    1, 2, 4, 8], 'ffn_expansion_factor': 2.66, 'bias': False, 'LayerNorm_type': 'WithBias', 'dual_pixel_task': False}
+img_multiple_of = 8
+
+
+def motion_deblurring(img):
+    weights = os.path.join('weigths', 'motion_deblurring.pth')
+    checkpoint = torch.load(weights)
+    device = torch.device('cpu')
+    model = Restormer(**parameters)
+    model.load_state_dict(checkpoint['params'])
+    model.eval()
+    with torch.no_grad():
+        input_ = torch.from_numpy(img).float().div(
+            255.).permute(2, 0, 1).unsqueeze(0).to(device)
+
+        # Pad the input if not_multiple_of 8
+        height, width = input_.shape[2], input_.shape[3]
+        H, W = ((height + img_multiple_of) // img_multiple_of) * \
+            img_multiple_of, ((width + img_multiple_of) //
+                              img_multiple_of) * img_multiple_of
+        padh = H - height if height % img_multiple_of != 0 else 0
+        padw = W - width if width % img_multiple_of != 0 else 0
+        input_ = F.pad(input_, (0, padw, 0, padh), 'reflect')
+        restored = model(input_)
+        restored = torch.clamp(restored, 0, 1)
+        # Unpad the output
+        restored = restored[:, :, :height, :width]
+
+        restored = restored.permute(0, 2, 3, 1).cpu().detach().numpy()
+        restored = img_as_ubyte(restored[0])
+        return restored
+
+
+def denoising(img):
+    parameters['LayerNorm_type'] = 'BiasFree'
+    weights = os.path.join('weigths', 'real_denoising.pth')
+    model = Restormer(**parameters)
+    device = torch.device('cpu')
+    checkpoint = torch.load(weights)
+    model.load_state_dict(checkpoint['params'])
+    model.eval()
+
+    with torch.no_grad():
+        input_ = torch.from_numpy(img).float().div(
+            255.).permute(2, 0, 1).unsqueeze(0).to(device)
+
+        # Pad the input if not_multiple_of 8
+        height, width = input_.shape[2], input_.shape[3]
+        H, W = ((height + img_multiple_of) // img_multiple_of) * \
+            img_multiple_of, ((width + img_multiple_of) //
+                              img_multiple_of) * img_multiple_of
+        padh = H - height if height % img_multiple_of != 0 else 0
+        padw = W - width if width % img_multiple_of != 0 else 0
+        input_ = F.pad(input_, (0, padw, 0, padh), 'reflect')
+        restored = model(input_)
+        restored = torch.clamp(restored, 0, 1)
+        # Unpad the output
+        restored = restored[:, :, :height, :width]
+
+        restored = restored.permute(0, 2, 3, 1).cpu().detach().numpy()
+        restored = img_as_ubyte(restored[0])
+        return restored
+
+
+def processAI(img):
+    res = motion_deblurring(img)
+    res = denoising(res)
+    return res
---
+++ final_proj/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, render_template, send_file, flash
+import cv2
+from ai import processAI
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = 'dlgkals2633'
+
+image = ""
+ai = ""
+
+
+@app.route('/', methods=["GET"])
+def homepage():
+    return render_template("index.html", image_file="picture.jpg", result_file="picture.jpg")
+
+
+@app.route('/upload', methods=["POST", "GET"])
+def upload_file():
+    global image
+    if request.method == "POST":
+        try:
+            data = request.files['file']
+            image = request.files['file']
+            data.save('./static/'+secure_filename(data.filename))
+            return render_template("index.html", image_file=data.filename, result_file="picture.jpg")
+        except:
+            flash("image를 넣고 UPLOAD 하세요!")
+            return render_template("index.html", image_file="picture.jpg", result_file="picture.jpg")
+    else:
+        return render_template("index.html", image_file="picture.jpg", result_file="picture.jpg")
+
+
+@app.route('/fileDown', methods=['GET', 'POST'])
+def download_file():
+    if request.method == 'POST':
+        try:
+            path = "./static/"
+            return send_file(path + 'result.jpg',
+                             as_attachment=True)
+        except:
+            flash("Download할 파일이 없습니다!")
+            return render_template("index.html", image_file="picture.jpg")
+    return render_template("index.html", image_file=image.filename)
+
+
+@app.route('/process', methods=["POST", "GET"])
+def processing():
+    if request.method == "POST":
+        # try:
+        img = cv2.imread('static/'+image.filename)
+        ai = processAI(img)
+        cv2.imwrite('static/result.jpg', ai)
+        return render_template("index.html", result_file="result.jpg", image_file=image.filename)
+        # except:
+        #     flash("Restomer를 실패하였습니다.")
+        #     return render_template("index.html", image_file="picture.jpg")
+    else:
+        return render_template("index.html", image_file="picture.jpg")
+
+
+if __name__ == "__main__":
+    app.run(use_reloader=False)
