40445028a8daa2af19cb61b8219538616b70515c
Hamin <hamin2633@naver.com>
2023-02-14 16:52:45 +0900

    주소 변경

--- final_proj/app.py
+++ final_proj/app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 app.secret_key = 'dlgkals2633'
 
 image = ""
-ai = ""
 
 
 @app.route('/', methods=["GET"])
@@ -60,4 +59,4 @@ def processing():
 
 
 if __name__ == "__main__":
-    app.run(use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
